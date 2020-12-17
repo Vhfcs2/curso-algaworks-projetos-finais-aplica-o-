a27e062f20b77d99d0f905425cbfe10ac4e16fca
Victor Hugo <Victor Hugo@MSALVADOR-DESKTOP>
2020-12-17 12:29:23 -0800

    capítulo 13 do curso de lógica de programação

---
+++ AlgaWorkscontato/src/main/java/com/algaworks/AlgaWorkscontatoApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaWorkscontatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaWorkscontatoApplication.class, args);
+	}
+
+}
